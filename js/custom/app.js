3e35af914c279ea4925f3ed8969f3a6b272bbbfc
Unknown <mou.wang@104.com.tw>
2018-01-01 17:28:02 +0800

    Update JS.

--- js/custom/app.js
+++ js/custom/app.js
@@ -1,14 +1,19 @@
 'use strict';
 
-console.log('Hello this is app.js 1631');
+console.log('Hello this is app.js 1634');
 
 var SwingEventApi = ' https://f2qiac05il.execute-api.us-west-2.amazonaws.com/master';
+var inputType = 'swing';
 
 $.ajax({
     method: "POST",
     url: SwingEventApi,
-    data: { name: "John", location: "Boston" }
+    contents: { 'type' : 'swing'},
+    data: JSON.stringify ({ type: inputType}),
+    dataType: 'json',
+    contentType: 'application/json'
 })
 .done(function( msg ) {
-    alert( "Api Result " + msg );
+    //alert( "Api Result " + msg );
+    console.log(msg);
 });
