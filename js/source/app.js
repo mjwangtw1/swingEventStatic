bfc5e12642d67c2aa28460e524c170f02c35f585
Mou-Chun Wang <jimmyexx@gmail.com>
2018-04-04 18:44:04 +0800

    use jQuery Template stuff

--- js/source/app.js
+++ js/source/app.js
@@ -1,7 +1,6 @@
 
 'use strict';
-
-console.log('Hello this is app.js 1634');
+console.log('Hello this is app.js 1843');
 
 var SwingEventApi = ' https://f2qiac05il.execute-api.us-west-2.amazonaws.com/master';
 var inputType = 'swing';
@@ -16,5 +15,21 @@ $.ajax({
 })
 .done(function( msg ) {
     //alert( "Api Result " + msg );
-    console.log(msg);
+    //console.log();
+
+    var allList = msg.body.items;
+    var regular_Events = allList[2];
+    var hotEvent = allList[4];
+    var suggEvent = allList[7];
+    console.log(allList);
+
+    //Mount Hit Events
+    $("#template_hot").tmpl(hotEvent).appendTo("#hot_event");
+
+    //Mount Regular Events - here add loops later
+    $("#template_items").tmpl(regular_Events).appendTo(".mentionEventList");
+
+    //Mount the Rest
+    $("#template_sugg").tmpl(suggEvent).appendTo(".allEventList");
+
 });
