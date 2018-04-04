cfe85146460cfe860e50f6a9986111819c9b1a1c
yuming <yuming@thepolydice.com>
2018-04-04 12:46:29 +0800

    Include previous js

---
+++ js/source/app.js
@@ -0,0 +1,20 @@
+
+'use strict';
+
+console.log('Hello this is app.js 1634');
+
+var SwingEventApi = ' https://f2qiac05il.execute-api.us-west-2.amazonaws.com/master';
+var inputType = 'swing';
+
+$.ajax({
+    method: "POST",
+    url: SwingEventApi,
+    contents: { 'type' : 'swing'},
+    data: JSON.stringify ({ type: inputType}),
+    dataType: 'json',
+    contentType: 'application/json'
+})
+.done(function( msg ) {
+    //alert( "Api Result " + msg );
+    console.log(msg);
+});
